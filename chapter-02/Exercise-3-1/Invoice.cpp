12b2863bf9c576ea9214d2f549bc80346b7ea78c
mishokura <79420179+mishokura@users.noreply.github.com>
2022-10-03 18:54:12 +0400

    3

--- chapter-02/Exercise-3-1/Invoice.cpp
+++ chapter-02/Exercise-3-1/Invoice.cpp
@@ -7,40 +7,63 @@ int main()
 {
 	cout << "Welcome to Invoice Program" << endl << endl;
 	
+	A:
 	//customer type
 	char type;
 	cout << "Enter customer type(r/w/c):          ";
 	cin >> type;
 
+	//check if costomer type exists
+	if (tolower(type) != 'r') {
+		cout << "customer type not found, try again." << endl;
+			goto A;
+	}
+	else {goto B; 
+	}
+	if (tolower(type) != 'w') {
+		cout << "customer type not found, try again." << endl;
+		goto A;
+	}
+	else {
+		goto B;
+	}
+	if (tolower(type) != 'c') {
+		cout << "customer type not found, try again." << endl;
+		goto A;
+	}
+	else {
+		goto B;
+	}
+
+	B:
 	//get subtotal
 	double subtotal;
 	cout << "enter subtotal:        ";
 	cin >> subtotal;
 
-	//customer r
-	double discount = .0;
-	if (tolower(type) == 'r' && subtotal > 250 && subtotal < 500) {
-		discount = .2;
-	} else if (tolower(type) == 'r' && subtotal > 500) {
-		discount = .3;
-	}
-	
-	//customer w        !!! aq ar vici ra safasurze ra fasdakleba unda yofiliyo da prosta 100 da 300 chavsvi.
-	if (tolower(type) == 'w' && subtotal > 100 && subtotal < 300) {
-		discount = .2;
-	}
-	else if (tolower(type) == 'w' && subtotal > 500) {
-		discount = .3;
-	}
+		//customer r
+		double discount = .0;
+		if (tolower(type) == 'r' && subtotal > 250 && subtotal < 500) {
+			discount = .2;
+		}
+		else if (tolower(type) == 'r' && subtotal > 500) {
+			discount = .3;
+		}
 
-	//customer c
-	if (tolower(type) == 'c') {
-		discount = .25;
-	}
+		//customer w        !!! aq ar vici ra safasurze ra fasdakleba unda yofiliyo da prosta 100 da 300 chavsvi.
+		if (tolower(type) == 'w' && subtotal > 100 && subtotal < 300) {
+			discount = .2;
+		}
+		else if (tolower(type) == 'w' && subtotal > 500) {
+			discount = .3;
+		}
 
+		//customer c
+		if (tolower(type) == 'c') {
+			discount = .25;
+		}
 
 	//calculations
-	// 
 	//discount amount
 	double total;
 	total = subtotal - subtotal * discount;
